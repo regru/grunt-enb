2cd510c2d9228bbea4c3a7a8713cff9e227bf8b0
Makarychev Andrey <makarychev@reg.ru>
2015-11-16 21:17:30 +0400

    cs

--- tasks/enb.js
+++ tasks/enb.js
@@ -15,26 +15,28 @@ module.exports = function(grunt) {
     var enbBuilder = require('enb/lib/api/make');
 
     grunt.registerMultiTask('enb', 'enb make for project', function () {
-        if (!this.data.targets)
+        if (!this.data.targets) {
             return grunt.log.error('no targets provided');
+        }
 
-        var done = this.async(),
-            options = extend(this.options({
-                    noLog: false
-                }), {
-                    cdir: resolve(this.options().projectPath || './')
-                }
-            ),
-            _this = this;
+        var done = this.async();
+        var defaults = {
+            noLog: false,
+            dir: resolve(this.options().projectPath || '')
+        };
+        var options = this.options(defaults);
+        var _this = this;
 
         this.data.env && extend(process.env, this.data.env);
         this.data.beforeBuild && this.data.beforeBuild();
-        enbBuilder(this.data.targets, options).then(function () {
-            grunt.log.write('enb make...').ok();
-            _this.data.afterBuild && _this.data.afterBuild();
-            done();
-        }, function (err) {
-            return grunt.log.error(err);
-        });
+
+        enbBuilder(this.data.targets, options)
+            .then(function () {
+                grunt.log.write('enb make...').ok();
+                _this.data.afterBuild && _this.data.afterBuild();
+                done();
+            }, function (err) {
+                return grunt.log.error(err);
+            });
     });
 };
